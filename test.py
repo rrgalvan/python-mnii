f8d523573712da450b963240fcb6bc92b8d182d0
rrgalvan <rafa.rodriguez.galvan@gmail.com>
2015-03-04 22:13:24 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+f = lambda x: x**3 + 4*x**2 -3*x -5
+
+a, b = 1, 2
+
+assert f(a)*f(b)<0,  "No se verifican hipótesis del T. Bolzano"
+
+kmax = 6
+for k in range(kmax):
+    c = 0.5*(a+b)
+    print "[a,b]=[%f,%f], c=%f" % (a,b,c)    
+    if f(a)*f(c)<0:
+        b=c
+    else:
+        a=c
